a107cc346accf3cf6020a98df5e3a45828d69a7b
Jonkim <kanggokku1@gmail.com>
2022-01-31 22:59:44 +0900

    3rd commit

--- 리포트 정리.py
+++ 리포트 정리.py
@@ -1,4 +1,3 @@
-from ast import NotIn
 import os
 from os import walk
 import pandas as pd
@@ -38,12 +37,13 @@ for j in f_name:
         # 필요없는 열 삭제
         df.drop(["Status", "Username", "Password", "E-Mail"],
                 axis="columns", inplace=True)
-        # 필요없는 행 삭제
-        spac = df[df['Task ID'].str.contains('Task ID')].index
-        df.drop(spac, inplace=True)
+        # 필요없는 행 삭제 (아래 도메인 제거 코드에서 작업 가능 함)
+        # spac = df[df['Task ID'].str.contains('Task ID')].index
+        # df.drop(spac, inplace=True)
 
         # 제거할 도메인 리스트
-        drop_list = ["simplesite.com",
+        drop_list = ["Site URL",
+                     "simplesite.com",
                      "edublogs.org",
                      "penzu.com",
                      "storeboard.com",
@@ -57,19 +57,28 @@ for j in f_name:
             idx = df[df["Site URL"] == h].index     # Site URL 중에 j 값이 저장됨
             df.drop(idx, inplace=True)  # 해당 인덱스를 제거함
 
-
         # 인덱스 재정렬
         # df.reset_index(drop=True, inplace=True)
-
 
         # 최종 정리후 저장
         excel = excel.append(df, ignore_index=True)
 
+    # 데이터를 내림차순으로 정렬
+    excel = excel.sort_values(by=["Task ID"], ascending=True)
+    
+    # 저장 전 Tasks 순서대로 정렬하기   
+    excel_2 = pd.DataFrame()
+    Tasks = ["Web 2.0 Blogs", "Social Bookmarking", "Authority Links", "Edu", "URL Shortener"]
+
+    for t in Tasks:
+        temp_task = excel[excel["Task ID"].str.contains(t)]
+        excel_2 = excel_2.append(temp_task, ignore_index=True)
 
     # 최종 데이터의 총 row 수를 저장
-    las_column = len(excel)
+    las_column = len(excel_2)
 
-    excel.to_excel(f"{path_2}\\{j}{las_column}.xlsx", index=False)
+    # 엑셀로 정리하기 전에 정렬하기, 이후 꾸미기까지 
+    excel_2.to_excel(f"{path_2}\\{j}{las_column}.xlsx", index=False)
 
 
 # drop_list를 외부로 꺼내서 def 형식으로 불러오게 해야함
